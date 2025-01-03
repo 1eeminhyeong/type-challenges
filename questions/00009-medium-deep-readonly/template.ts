ef12d6f96cf4e0e7a443aacf05b2b28e33c04534
minhyung <peterlee11@naver.com>
2025-01-03 14:38:59 +0900

    9-medium-deep-readonly

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,1 +1,10 @@
-type DeepReadonly<T> = any
+// type DeepReadonly<T> = {
+//   readonly [P in keyof T]: T[P] extends object
+//     ? T[P] extends Function
+//       ? T[P]
+//       : DeepReadonly<T[P]>
+//     : T[P]
+// }
+type DeepReadonly<T> = keyof T extends never
+  ? T
+  : { readonly [key in keyof T]: DeepReadonly<T[key]> }
